d7edd7ab9ac594772fd5d5bb6fe5d9d944e81665
TGunderson <tgunderson@gmail.com>
2026-04-27 11:24:13 -0700

    ADO 9768

---
+++ caregiver-portal/src/pages/Privacy.jsx
@@ -0,0 +1,17 @@
+// src/pages/ContactUs.jsx
+import "../DesignTokens.css";
+
+export default function Privacy() { 
+
+ return (
+    <div className="page">
+                <div className="page-details">
+                <div className="page-details-row">
+        <h2 className="page-heading">Privacy</h2>
+        </div>
+        </div>
+    </div>
+
+ );
+
+}
