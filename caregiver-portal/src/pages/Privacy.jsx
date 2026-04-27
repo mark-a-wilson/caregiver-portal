d3797e0030fb5723d99f098b44690723f779460b
Mark Wilson <Mark.Wilson@gov.bc.ca>
2026-04-27 14:05:43 -0700

    Add content for Privacy page

--- caregiver-portal/src/pages/Privacy.jsx
+++ caregiver-portal/src/pages/Privacy.jsx
@@ -5,11 +5,29 @@ export default function Privacy() {
 
  return (
     <div className="page">
-                <div className="page-details">
-                <div className="page-details-row">
-        <h2 className="page-heading">Privacy</h2>
-        </div>
-        </div>
+      <div className="page-details">
+         <div className="page-details-row">
+            <h2 className="page-heading">Privacy</h2>
+            <p className="bold">Introduction</p>
+            <p>The British Columbia (B.C.) Government is committed to protecting your privacy. The B.C. government collects, uses and discloses your personal information in accordance with the <a href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/96165_00" target="_blank" className="italic hyperlink">Freedom of Information and Protection of Privacy Act</a> <span className="italic">(FOIPPA)</span> and other applicable legislation. ‘Personal information’ is defined broadly in the Freedom of Information and Protection of Privacy Act as recorded information about an identifiable individual, other than contact information, which is the information used to contact a person at a place of business. The purpose of this privacy statement is to inform you of the personal information that may be collected from you when visiting the Foster & Care Provider Portal (the Portal) and how that information may be used.</p>
+            <p className="bold">How is my personal information collected?</p>
+            <p>The Foster & Care Provider Portal (the Portal) collects information about your visit, including personal information, through the use of audit logs and cookies. A cookie is a small file stored on your computer by your web browser when using some websites.</p>
+            <p>The B.C. government, via its websites, will notify you if cookies will be collecting additional information from you. A cookie may remain on your computer after the Internet session finishes (until the cookie expires or is deleted by you).</p>
+            <p>The B.C. government collects personal information under the authority of section 26(c) of <span className="italic">FOIPPA</span> for the purposes stated below in the next section.</p>
+            <p className="bold">What is the purpose for collecting my personal information and how will it be used?</p>
+            <p>Any information you provide through web forms in the Portal will be collected by the Ministry of Children and Family Development (MCFD) for the purpose of facilitating your application to become a caregiver/care provider and be involved in the provision of care to children for MCFD.</p>
+            <p className="bold">Can I opt out of the collection of my personal information?</p>
+            <p>Your browser may permit you to disable cookies, but your decision to disable cookies may impair your ability to browse, read, and download information contained on B.C. government websites, and government may be unable to personalize your web experience. However, you may still be able to access B.C. government services by other methods such as personal contact, fax or mail.</p>
+            <p className="bold">What safeguards are in place to protect my personal information?</p>
+            <p>The Government of B.C. is obligated to protect your personal information by making reasonable security arrangements against such risks as unauthorized access, collection, use, disclosure or disposal. Specifically, access to systems, applications and the collected data is restricted to authorized personnel only. Furthermore, any personal information collected and used for identifying user trends (e.g. IP address) is aggregated and made anonymous during report generation.</p>
+            <p className="bold">How long is information retained?</p>
+            <p>Cookies will remain on your computer for only as long as your browser remains open, or until you delete them off your computer. Information collected as a part of a cookie or a security audit log is retained for 2 years. Information collected or created by the Government of B.C. is maintained in accordance with <a href="http://www.gov.bc.ca/citz/iao/records_mgmt/" target="_blank" className="hyperlink">government's records retention schedules</a> and other legislative requirements.</p>
+            <p className="bold">How can I access and correct information I submit to the Foster & Care Provider Portal?</p>
+            <p>You can review any personal information you've submitted to the Portal by contacting <a href="mailto:MCF.CentralizedRetentionandRecruitment@gov.bc.ca" className="hyperlink">MCF.CentralizedRetentionandRecruitment@gov.bc.ca</a>, or by submitting a Freedom of Information request. You may request changes or annotate your personal information if you believe it to be inaccurate by submitting a written request describing the error. </p>
+            <p className="bold">Who can I contact for more information about this privacy statement?</p>
+            <p>Questions regarding this privacy statement, including the collection of personal information, can be directed to a Senior Privacy Advisor at the Privacy, Compliance and Training Branch in the Ministry of Citizens’ Services, PO Box 9406 Stn Prov Govt, Victoria BC, V8W9V1, telephone (250) 356-1851. <a href="mailto:Privacy.Helpline@gov.bc.ca" className="hyperlink">Privacy.Helpline@gov.bc.ca</a>.</p>
+         </div>
+      </div>
     </div>
 
  );
